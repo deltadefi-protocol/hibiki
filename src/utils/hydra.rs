fa9c1616eb9e78e2e38ac885b498daf045bb35e1
SIDANWhatever <wongkahinhinson@gmail.com>
2025-06-30 21:28:17 +0800

    feat: internal transfer post-debugging

--- src/utils/hydra.rs
+++ src/utils/hydra.rs
@@ -3,11 +3,12 @@ use whisky::{OfflineTxEvaluator, TxBuilder, TxBuilderParam};
 use crate::config::hydra::get_hydra_pp;
 
 pub fn get_hydra_tx_builder() -> TxBuilder {
-    let tx_builder = TxBuilder::new(TxBuilderParam {
+    let mut tx_builder = TxBuilder::new(TxBuilderParam {
         evaluator: Some(Box::new(OfflineTxEvaluator::new())),
         fetcher: None,
         submitter: None,
         params: Some(get_hydra_pp()),
     });
+    tx_builder.serializer.tx_evaluation_multiplier_percentage = 150;
     tx_builder
 }
