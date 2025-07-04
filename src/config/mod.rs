871c428693c5608724c721a39ba7d464f0bad443
SIDANWhatever <wongkahinhinson@gmail.com>
2025-07-04 14:08:23 +0800

    feat: change mnemonic env to comma delimiter

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -11,7 +11,9 @@ impl AppConfig {
     pub fn new() -> AppConfig {
         AppConfig {
             network_id: var("NETWORK_ID").unwrap_or("0".to_string()),
-            app_owner_mnemonic: var("APP_OWNER_SEED_PHRASE").unwrap(),
+            app_owner_mnemonic: convert_mnemonic_comma_to_space(
+                &var("APP_OWNER_SEED_PHRASE").unwrap(),
+            ),
             app_owner_vkey: var("OWNER_VKEY").unwrap_or("".to_string()),
         }
     }
@@ -20,5 +22,20 @@ impl AppConfig {
 impl Default for AppConfig {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+fn convert_mnemonic_comma_to_space(mnemonic: &str) -> String {
+    mnemonic.replace(',', " ")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_mnemonic_comma_conversion() {
+        let mnemonic = "solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution,solution";
+        let expected_mnemonic = "solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution solution";
+        let converted = super::convert_mnemonic_comma_to_space(mnemonic);
+        assert_eq!(converted, expected_mnemonic);
     }
 }
