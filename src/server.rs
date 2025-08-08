a95611f8d6798ea7d78a1005266274ad589c6bf0
Hinson <90688504+HinsonSIDAN@users.noreply.github.com>
2025-08-08 09:43:08 +0800

    Feature/improve sign tx latency (#9)
    * fix: sign tx latency + adding time log for sign tx
    
    * fix: sign tx latency + adding time log for sign tx

--- src/server.rs
+++ src/server.rs
@@ -13,6 +13,7 @@ use hibiki::{
         TxHashResponse,
     },
 };
+use std::time::Instant;
 use tonic::{transport::Server, Request, Response, Status};
 
 #[derive(Debug, Default)]
@@ -98,6 +99,7 @@ impl Hibiki for HibikiService {
         &self,
         request: Request<services::SignTransactionRequest>,
     ) -> Result<Response<services::SignTransactionResponse>, Status> {
+        let start = Instant::now();
         println!("Got a request - sign_transaction");
         let request_result = request.into_inner();
         let reply = match sign_transaction::handler(request_result) {
@@ -106,6 +108,7 @@ impl Hibiki for HibikiService {
                 return Err(Status::failed_precondition(e.to_string()));
             }
         };
+        println!("Time taken for sign_transaction: {:?}", start.elapsed());
         Ok(Response::new(reply))
     }
 
@@ -113,6 +116,7 @@ impl Hibiki for HibikiService {
         &self,
         request: Request<services::SignTransactionRequest>,
     ) -> Result<Response<services::SignTransactionResponse>, Status> {
+        let start = Instant::now();
         println!("Got a request - sign_transaction_with_fee_collector");
         let request_result = request.into_inner();
         let reply = match sign_transaction_with_fee_collector::handler(request_result) {
@@ -121,6 +125,10 @@ impl Hibiki for HibikiService {
                 return Err(Status::failed_precondition(e.to_string()));
             }
         };
+        println!(
+            "Time taken for sign_transaction_with_fee_collector: {:?}",
+            start.elapsed()
+        );
         Ok(Response::new(reply))
     }
 
--- src/utils/wallet.rs
+++ src/utils/wallet.rs
@@ -4,11 +4,11 @@ use whisky::Wallet;
 pub fn get_app_owner_wallet() -> Wallet {
     let app_config = AppConfig::new();
     let owner_mnemonic = app_config.app_owner_mnemonic;
-    Wallet::new_mnemonic(&owner_mnemonic)
+    Wallet::new_mnemonic(&owner_mnemonic).expect("Failed to create app owner wallet")
 }
 
 pub fn get_fee_collector_wallet() -> Wallet {
     let app_config = AppConfig::new();
     let owner_mnemonic = app_config.fee_collector_mnemonic;
-    Wallet::new_mnemonic(&owner_mnemonic)
+    Wallet::new_mnemonic(&owner_mnemonic).expect("Failed to create fee collector wallet")
 }
