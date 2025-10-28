4af7aa29fb72eff86b5a8d570bf618bc1559520a
SIDANWhatever <wongkahinhinson@gmail.com>
2025-10-28 12:52:38 +0000

    fix: check signature

--- src/server.rs
+++ src/server.rs
@@ -1,35 +1,35 @@
 use dotenv::dotenv;
 use std::env;
-use whisky::{ calculate_tx_hash, Wallet };
+use std::sync::Arc;
+use whisky::{calculate_tx_hash, Wallet};
 
 use hibiki::{
-    handler::{
-        create_hydra_account_utxo,
-        internal_transfer,
-        process_transfer,
-        serialize_transfer_intent_datum,
-        sign_transaction,
-        sign_transaction_with_fee_collector,
-    },
     grpc_metrics_interceptor::MetricsLayer,
-    metrics,
-    metrics_server,
-    services::{ self, hibiki_server::{ Hibiki, HibikiServer }, TxHashResponse },
-    utils::wallet::{ get_app_owner_wallet, get_fee_collector_wallet },
+    handler::{
+        create_hydra_account_utxo, internal_transfer, process_transfer,
+        serialize_transfer_intent_datum, sign_transaction, sign_transaction_with_fee_collector,
+    },
+    metrics, metrics_server,
+    services::{
+        self,
+        hibiki_server::{Hibiki, HibikiServer},
+        TxHashResponse,
+    },
+    utils::wallet::{get_app_owner_wallet, get_fee_collector_wallet},
 };
 use std::time::Instant;
-use tonic::{ transport::Server, Request, Response, Status };
+use tonic::{transport::Server, Request, Response, Status};
 
 pub struct HibikiService {
-    pub app_owner_wallet: Wallet,
-    pub fee_collector_wallet: Wallet,
+    pub app_owner_wallet: Arc<Wallet>,
+    pub fee_collector_wallet: Arc<Wallet>,
 }
 
 #[tonic::async_trait]
 impl Hibiki for HibikiService {
     async fn ping_hello(
         &self,
-        _request: Request<services::HelloRequest>
+        _request: Request<services::HelloRequest>,
     ) -> Result<Response<services::HelloResponse>, Status> {
         let reply = services::HelloResponse {
             message: "Hello from Hibiki!".to_string(),
@@ -39,7 +39,7 @@ impl Hibiki for HibikiService {
 
     async fn internal_transfer(
         &self,
-        request: Request<services::InternalTransferRequest>
+        request: Request<services::InternalTransferRequest>,
     ) -> Result<Response<services::IntentTxResponse>, Status> {
         let request_result = request.into_inner();
         println!("Got a request - internal_transfer {:?}", request_result);
@@ -55,7 +55,7 @@ impl Hibiki for HibikiService {
 
     async fn process_transfer(
         &self,
-        request: Request<services::ProcessTransferRequest>
+        request: Request<services::ProcessTransferRequest>,
     ) -> Result<Response<services::ProcessTransferResponse>, Status> {
         let request_result = request.into_inner();
         println!("Got a request - process_transfer {:?}", request_result);
@@ -70,12 +70,15 @@ impl Hibiki for HibikiService {
 
     async fn create_hydra_account_utxo(
         &self,
-        request: Request<services::CreateHydraAccountUtxoRequest>
+        request: Request<services::CreateHydraAccountUtxoRequest>,
     ) -> Result<Response<services::CreateHydraAccountUtxoResponse>, Status> {
         let request_result = request.into_inner();
-        println!("Got a request - create_hydra_account_utxo {:?}", request_result);
-        let reply = match
-            create_hydra_account_utxo::handler(request_result, &self.app_owner_wallet).await
+        println!(
+            "Got a request - create_hydra_account_utxo {:?}",
+            request_result
+        );
+        let reply = match create_hydra_account_utxo::handler(request_result, &self.app_owner_wallet)
+            .await
         {
             Ok(value) => value,
             Err(e) => {
@@ -87,7 +90,7 @@ impl Hibiki for HibikiService {
 
     async fn serialize_transferal_intent_datum(
         &self,
-        request: Request<services::SerializeTransferalIntentDatumRequest>
+        request: Request<services::SerializeTransferalIntentDatumRequest>,
     ) -> Result<Response<services::SerializeDatumResponse>, Status> {
         println!("Got a request - serialize_transferal_intent_datum");
         let request_result = request.into_inner();
@@ -102,7 +105,7 @@ impl Hibiki for HibikiService {
 
     async fn sign_transaction(
         &self,
-        request: Request<services::SignTransactionRequest>
+        request: Request<services::SignTransactionRequest>,
     ) -> Result<Response<services::SignTransactionResponse>, Status> {
         let start = Instant::now();
         println!("Got a request - sign_transaction");
@@ -119,26 +122,30 @@ impl Hibiki for HibikiService {
 
     async fn sign_transaction_with_fee_collector(
         &self,
-        request: Request<services::SignTransactionRequest>
+        request: Request<services::SignTransactionRequest>,
     ) -> Result<Response<services::SignTransactionResponse>, Status> {
         let start = Instant::now();
         println!("Got a request - sign_transaction_with_fee_collector");
         let request_result = request.into_inner();
-        let reply = match
-            sign_transaction_with_fee_collector::handler(request_result, &self.fee_collector_wallet)
-        {
+        let reply = match sign_transaction_with_fee_collector::handler(
+            request_result,
+            &self.fee_collector_wallet,
+        ) {
             Ok(value) => value,
             Err(e) => {
                 return Err(Status::failed_precondition(e.to_string()));
             }
         };
-        println!("Time taken for sign_transaction_with_fee_collector: {:?}", start.elapsed());
+        println!(
+            "Time taken for sign_transaction_with_fee_collector: {:?}",
+            start.elapsed()
+        );
         Ok(Response::new(reply))
     }
 
     async fn calculate_tx_hash(
         &self,
-        request: Request<services::CalculateTxHashRequest>
+        request: Request<services::CalculateTxHashRequest>,
     ) -> Result<Response<services::TxHashResponse>, Status> {
         println!("Got a request - calculate_tx_hash");
         let request_result = request.into_inner();
@@ -160,16 +167,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     metrics::init_metrics();
 
     let grpc_port = env::var("PORT").unwrap_or_else(|_| "50051".to_string());
-    let metrics_port: u16 = env
-        ::var("METRICS_PORT")
+    let metrics_port: u16 = env::var("METRICS_PORT")
         .unwrap_or_else(|_| "9090".to_string())
         .parse()
         .expect("METRICS_PORT must be a valid port number");
 
     let grpc_addr = format!("0.0.0.0:{}", grpc_port).parse()?;
     let transactions = HibikiService {
-        app_owner_wallet: get_app_owner_wallet(),
-        fee_collector_wallet: get_fee_collector_wallet(),
+        app_owner_wallet: Arc::new(get_app_owner_wallet()),
+        fee_collector_wallet: Arc::new(get_fee_collector_wallet()),
     };
 
     println!("gRPC Server listening on port {}...", grpc_port);
@@ -186,7 +192,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .layer(MetricsLayer)
         .add_service(HibikiServer::new(transactions))
-        .serve(grpc_addr).await?;
+        .serve(grpc_addr)
+        .await?;
 
     Ok(())
 }
