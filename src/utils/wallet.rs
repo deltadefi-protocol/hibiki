231e18da45502114abe482a9835f2ac3111bd5d0
Ken Lau <kenlauhong2015@gmail.com>
2025-11-06 14:30:06 +0800

    fix: wallet hardcode network id = 0

--- src/utils/wallet.rs
+++ src/utils/wallet.rs
@@ -1,14 +1,38 @@
 use crate::config::AppConfig;
-use whisky::Wallet;
+use whisky::{NetworkId, Wallet};
 
 pub fn get_app_owner_wallet() -> Wallet {
     let app_config = AppConfig::new();
     let owner_mnemonic = app_config.app_owner_mnemonic;
-    Wallet::new_mnemonic(&owner_mnemonic).expect("Failed to create app owner wallet")
+    let network_id = match app_config
+        .network_id
+        .parse::<u8>()
+        .expect("Failed to parse network_id")
+    {
+        0 => NetworkId::Preprod,
+        1 => NetworkId::Mainnet,
+        _ => NetworkId::Preprod, // Default to Preprod
+    };
+
+    Wallet::new_mnemonic(&owner_mnemonic)
+        .expect("Failed to create app owner wallet")
+        .with_network_id(network_id)
 }
 
 pub fn get_fee_collector_wallet() -> Wallet {
     let app_config = AppConfig::new();
     let owner_mnemonic = app_config.fee_collector_mnemonic;
-    Wallet::new_mnemonic(&owner_mnemonic).expect("Failed to create fee collector wallet")
+    let network_id = match app_config
+        .network_id
+        .parse::<u8>()
+        .expect("Failed to parse network_id")
+    {
+        0 => NetworkId::Preprod,
+        1 => NetworkId::Mainnet,
+        _ => NetworkId::Preprod, // Default to Preprod
+    };
+
+    Wallet::new_mnemonic(&owner_mnemonic)
+        .expect("Failed to create fee collector wallet")
+        .with_network_id(network_id)
 }
