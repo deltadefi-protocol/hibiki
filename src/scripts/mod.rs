2c9614ee24bed2b3f7a60b7f243d5fcf2aca11cb
Ken Lau <kenlauhong2015@gmail.com>
2025-12-08 16:09:37 +0800

    Revert "fix: remove create hydra account"
    This reverts commit 800fc24c43c7567ccb95422309f43b032006c33f.

--- src/scripts/mod.rs
+++ src/scripts/mod.rs
@@ -1,8 +1,4 @@
-pub mod hydra_user_intent;
-pub use hydra_user_intent::*;
 pub mod types;
 pub use types::*;
-pub mod account_operation;
-pub use account_operation::*;
-pub mod hydra_account_balance;
-pub use hydra_account_balance::*;
+pub mod bar;
+pub use bar::*;
--- src/scripts/types/account.rs
+++ src/scripts/types/account.rs
@@ -1,43 +1,132 @@
-use serde_json::Value;
-use whisky::{
-    data::{ByteString, Constr0, Credential},
-    impl_constr_wrapper_type,
-};
-use whisky::{ConstrEnum, ConstrWrapper};
+use whisky::data::{ByteString, Constr0, Constr1, Constr2, Credential, PolicyId, ScriptHash};
 
-#[derive(Debug, Clone, ConstrEnum)]
-pub enum UserAccount {
-    UserSpotAccount(Account),
-    UserFundingAccount(Account),
-    UserMobileAccount(Account),
+use crate::{
+    constant::{dex_oracle_nft, SCRIPTS},
+    scripts::{
+        bar::{
+            Account, CancelWithdrawalIntent, HydraAccountIntent, HydraOrderBookRedeemer,
+            HydraUserIntentRedeemer, MValue, MintMasterIntent, TransferIntent, UserAccount,
+            UserFundingAccount, UserMobileAccount, UserTradeAccount, WithdrawalIntent,
+        },
+        MasterIntent,
+    },
+};
+
+impl Account {
+    pub fn new_from_keys(
+        account_id: ByteString,
+        master_key: (&str, bool),
+        operation_key: (&str, bool),
+    ) -> Self {
+        let master_credential = Credential::new(master_key);
+        let operation_credential = Credential::new(operation_key);
+
+        Account(Constr0::new(Box::new((
+            account_id,
+            master_credential,
+            operation_credential,
+        ))))
+    }
 }
 
-#[derive(Debug, Clone, ConstrWrapper)]
-pub struct Account(Constr0<Box<(ByteString, Credential, Credential)>>);
-impl_constr_wrapper_type!(Account, 0, [
-  (account_id: ByteString, &str),
-  (master_key: Credential, (&str, bool)),
-  (operation_key: Credential, (&str, bool)),
-]);
+impl UserTradeAccount {
+    pub fn new_from_account<P>(
+        account: Account,
+        hydra_order_book_withdrawal_blueprint: fn(
+            PolicyId,
+        ) -> whisky::WithdrawalBlueprint<
+            P,
+            HydraOrderBookRedeemer,
+        >,
+    ) -> UserAccount
+    where
+        P: whisky::data::PlutusDataJson,
+    {
+        let policy_id = PolicyId::new(dex_oracle_nft());
+        let blueprint = hydra_order_book_withdrawal_blueprint(policy_id);
+        let script_hash = ScriptHash::new(&blueprint.hash);
+
+        UserAccount::UserTradeAccount(UserTradeAccount(Constr0::new(Box::new((
+            account,
+            script_hash,
+        )))))
+    }
+}
 
 impl UserAccount {
     pub fn from_proto(account_info: &hibiki_proto::services::AccountInfo) -> Self {
-        // Remove dashes from UUID
         let clean_account_id = account_info.account_id.replace("-", "");
 
-        let account = Account::from(
-            &clean_account_id,
+        let account = Account::new_from_keys(
+            ByteString::new(&clean_account_id),
             (&account_info.master_key, account_info.is_script_master_key),
             (
                 &account_info.operation_key,
                 account_info.is_script_operation_key,
             ),
         );
+
+        let policy_id = PolicyId::new(dex_oracle_nft());
+        let blueprint = (SCRIPTS.hydra_order_book.withdrawal)(policy_id);
+        let script_hash = ScriptHash::new(&blueprint.hash);
+
         match account_info.account_type.as_str() {
-            "spot_account" => UserAccount::UserSpotAccount(account),
-            "funding_account" => UserAccount::UserFundingAccount(account),
-            "mobile_account" => UserAccount::UserMobileAccount(account),
-            _ => panic!("Unknown account type"),
+            "spot_account" => UserAccount::UserTradeAccount(UserTradeAccount(Constr0::new(
+                Box::new((account, script_hash)),
+            ))),
+            "funding_account" => UserAccount::UserFundingAccount(UserFundingAccount(Constr1::new(
+                Box::new((account, script_hash)),
+            ))),
+            "mobile_account" => UserAccount::UserMobileAccount(UserMobileAccount(Constr2::new(
+                Box::new((account, script_hash)),
+            ))),
+            _ => panic!("Unknown account type: {}", account_info.account_type),
         }
+    }
+}
+
+impl WithdrawalIntent {
+    pub fn new(value: MValue) -> HydraAccountIntent {
+        HydraAccountIntent::WithdrawalIntent(WithdrawalIntent(Constr0::new(value)))
+    }
+}
+
+impl CancelWithdrawalIntent {
+    pub fn new(value: MValue) -> HydraAccountIntent {
+        HydraAccountIntent::CancelWithdrawalIntent(CancelWithdrawalIntent(Constr1::new(value)))
+    }
+}
+
+impl TransferIntent {
+    pub fn new(account: UserAccount, value: MValue) -> HydraAccountIntent {
+        HydraAccountIntent::TransferIntent(TransferIntent(Constr2::new(Box::new((account, value)))))
+    }
+}
+
+impl HydraAccountIntent {
+    pub fn withdrawal(value: MValue) -> Self {
+        WithdrawalIntent::new(value)
+    }
+
+    pub fn cancel_withdrawal(value: MValue) -> Self {
+        CancelWithdrawalIntent::new(value)
+    }
+
+    pub fn transfer(account: UserAccount, value: MValue) -> Self {
+        TransferIntent::new(account, value)
+    }
+}
+
+impl MintMasterIntent {
+    pub fn new(account: UserAccount, intent: HydraAccountIntent) -> HydraUserIntentRedeemer {
+        HydraUserIntentRedeemer::MintMasterIntent(MintMasterIntent(Constr1::new(Box::new((
+            account, intent,
+        )))))
+    }
+}
+
+impl MasterIntent {
+    pub fn new(account: UserAccount, intent: HydraAccountIntent) -> MasterIntent {
+        MasterIntent(Constr1::new(Box::new((account, intent))))
     }
 }
