84d629b4a906f4f8a82b6dbe369fa7b80015c3c2
hinsonsidan <wongkahinhinson@gmail.com>
2026-01-20 00:28:31 +0800

    fix: fill order partial order type

--- src/scripts/types/order.rs
+++ src/scripts/types/order.rs
@@ -1,14 +1,20 @@
 use whisky::data::Int;
 
-use crate::scripts::Order;
+use crate::scripts::{Order, OrderType};
 
 impl Order {
     // Returns a new Order with updated price and size
     // Note: does not modify self, but returns a new instance
-    pub fn update_order(&self, order_size: u64, price_times_one_tri: u64) -> Order {
+    pub fn update_order(
+        &self,
+        order_size: u64,
+        price_times_one_tri: u64,
+        order_type: OrderType,
+    ) -> Order {
         let mut old_order = self.0.clone();
         old_order.fields.4 = Int::new(price_times_one_tri.into());
         old_order.fields.5 = Int::new(order_size.into());
+        old_order.fields.8 = order_type;
         Order(old_order)
     }
 }
